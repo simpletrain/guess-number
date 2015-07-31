f822e8a24bd7827f86119f48d0891ced4668a631
simpletrain <zsy498761554@live.com>
2015-07-31 17:44:38 +0800

    reformat compare-number-spec

--- spec/compare-number-spec.js
+++ spec/compare-number-spec.js
@@ -2,29 +2,29 @@
 
 var CompareNumber = require('../main/compare-number');
 
-describe('CompareNumber', function() {
+describe('CompareNumber', function () {
 
-  describe('#compare()',function(){
+    describe('#compare()', function () {
 
-    var compareNumber;
+        var compareNumber;
 
-    beforeEach(function(){
-      compareNumber = new CompareNumber();
+        beforeEach(function () {
+            compareNumber = new CompareNumber();
+        });
+
+        it('can return 0A0B', function () {
+            var result = compareNumber.compare('1234', '5678');
+            expect(result).toBe('0A0B');
+        });
+
+        it('can return 4A0B', function () {
+            var result = compareNumber.compare('1234', '1234');
+            expect(result).toBe('4A0B');
+        });
+
+        it('can return 0A4B', function () {
+            var result = compareNumber.compare('1234', '4321');
+            expect(result).toBe('0A4B');
+        });
     });
-
-    it('can return 0A0B',function(){
-      var result = compareNumber.compare('1234','5678');
-      expect(result).toBe('0A0B');
-    });
-
-    it('can return 4A0B',function(){
-      var result = compareNumber.compare('1234','1234');
-      expect(result).toBe('4A0B');
-    });
-
-    it('can return 0A4B',function(){
-      var result = compareNumber.compare('1234','4321');
-      expect(result).toBe('0A4B');
-    });
-  });
 });
