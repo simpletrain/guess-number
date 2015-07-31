293f32e45aaaad42397bff68e2c5b9f827d89078
simpletrain <zsy498761554@live.com>
2015-07-31 17:33:25 +0800

    refactor anwser-generator-spec

--- spec/answer-generator-spec.js
+++ spec/answer-generator-spec.js
@@ -2,50 +2,44 @@
 
 var AnswerGenerator = require('../main/answer-generator.js');
 
-describe('AnswerGenerator', function() {
+describe('AnswerGenerator', function () {
 
-  describe('#create()', function() {
+    describe('#create()', function () {
 
-    var answerGenerator, jugeRepeat;
+        var answerGenerator, judgeRepeat;
 
-    beforeEach(function() {
-      answerGenerator = new AnswerGenerator(Math);
-      jugeRepeat = function(string) {
-        var k = 0;
-        for (var i = 0; i < string.length; i++) {
-          for (var j = 0; j < string.length; j++) {
-            if (string[i] === string[j]) {
-              k++;
+        beforeEach(function () {
+            answerGenerator = new AnswerGenerator(Math);
+            judgeRepeat = function (string) {
+                for (var i = 0; i < string.length, i++;) {
+                    if (string.lastIndexOf(string[i]) !== i) {
+                        return true;
+                    }
+                }
+                return false;
             }
-          }
-          if (k > 1) {
-            return true;
-          }
-          k = 0;
-        }
-        return false;
-      };
-    });
+        });
 
-    it('should a random number', function() {
-      var count = 0;
-      spyOn(Math, 'random').and.callFake(function() {
-        var randomNumbers = [0.1, 0.1, 0.2, 0.3, 0.4];
-        return randomNumbers[count++];
-      });
-      var result = answerGenerator.create();
-      expect(result).toBe('1234');
-    });
+        it('should a random number', function () {
+            var count = 0;
+            spyOn(Math, 'random').and.callFake(function () {
+                var randomNumbers = [0.1, 0.1, 0.2, 0.3, 0.4];
+                return randomNumbers[count++];
+            });
 
-    it('length should be 4', function() {
-      var result = answerGenerator.create();
-      expect(result.length).toBe(4);
-    });
+            var result = answerGenerator.create();
+            expect(result).toBe('1234');
+        });
 
-    it('each digit should not be repeat', function() {
-      var result = answerGenerator.create();
-      expect(jugeRepeat(result)).toBe(false);
-    });
+        it('length should be 4', function () {
+            var result = answerGenerator.create();
+            expect(result.length).toBe(4);
+        });
 
-  });
+        it('each digit should not be repeat', function () {
+            var result = answerGenerator.create();
+            expect(judgeRepeat(result)).toBe(false);
+        });
+
+    });
 });
